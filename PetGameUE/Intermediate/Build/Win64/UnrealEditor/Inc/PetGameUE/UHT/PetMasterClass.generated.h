07cfb5e506fce39443dd87fd930a57e78f89b050
Harry Dandy <P2763046@my365.dmu.ac.uk>
2025-04-04 09:07:16 +0100

    PetClassMaster Created

---
+++ PetGameUE/Intermediate/Build/Win64/UnrealEditor/Inc/PetGameUE/UHT/PetMasterClass.generated.h
@@ -0,0 +1,64 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "PetMasterClass.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PETGAMEUE_PetMasterClass_generated_h
+#error "PetMasterClass.generated.h already included, missing '#pragma once' in PetMasterClass.h"
+#endif
+#define PETGAMEUE_PetMasterClass_generated_h
+
+#define FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_SPARSE_DATA
+#define FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_ACCESSORS
+#define FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAPetMasterClass(); \
+	friend struct Z_Construct_UClass_APetMasterClass_Statics; \
+public: \
+	DECLARE_CLASS(APetMasterClass, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/PetGameUE"), NO_API) \
+	DECLARE_SERIALIZER(APetMasterClass)
+
+
+#define FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APetMasterClass(APetMasterClass&&); \
+	NO_API APetMasterClass(const APetMasterClass&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APetMasterClass); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APetMasterClass); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(APetMasterClass) \
+	NO_API virtual ~APetMasterClass();
+
+
+#define FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_9_PROLOG
+#define FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_SPARSE_DATA \
+	FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_ACCESSORS \
+	FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_INCLASS_NO_PURE_DECLS \
+	FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PETGAMEUE_API UClass* StaticClass<class APetMasterClass>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_PetGameUE_Source_PetGameUE_Public_PetMasterClass_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ PetGameUE/Source/PetGameUE/Public/PetMasterClass.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PetMasterClass.generated.h"
+
+UCLASS()
+class PETGAMEUE_API APetMasterClass : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APetMasterClass();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
