453cb3cb399f47772f37263ef3577522102e833b
Harry Dandy <P2763046@my365.dmu.ac.uk>
2025-04-04 08:13:43 +0100

    Revert "Basic Input Setup"
    This reverts commit 26c44c604fd616f93ebdc5f589ab91ed393708d8.

---
+++ PetGameUE/Intermediate/Build/Win64/x64/PetGameUEEditor/Development/Core/SharedPCH.Core.Cpp17.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "Z:/Year Three/Module4/Unreal/PetGameUE/PetGameUE/Intermediate/Build/Win64/x64/PetGameUEEditor/Development/Core/SharedDefinitions.Core.Cpp17.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ PetGameUE/Intermediate/Build/Win64/x64/PetGameUEEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.36.32546
+#include "Z:/Year Three/Module4/Unreal/PetGameUE/PetGameUE/Intermediate/Build/Win64/x64/PetGameUEEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h"
