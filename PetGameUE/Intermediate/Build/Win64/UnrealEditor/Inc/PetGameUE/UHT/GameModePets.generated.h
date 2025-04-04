72b9e3cdb7fa7bf43f783e1bbc1efbd0f85b0b76
Harry Dandy <P2763046@my365.dmu.ac.uk>
2025-04-04 08:13:05 +0100

    Broke the whole thing

---
+++ PetGameUE/Intermediate/Build/Win64/UnrealEditor/Inc/PetGameUE/UHT/GameModePets.generated.h
@@ -0,0 +1,66 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "GameModePets.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PETGAMEUE_GameModePets_generated_h
+#error "GameModePets.generated.h already included, missing '#pragma once' in GameModePets.h"
+#endif
+#define PETGAMEUE_GameModePets_generated_h
+
+#define FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_SPARSE_DATA
+#define FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_ACCESSORS
+#define FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAGameModePets(); \
+	friend struct Z_Construct_UClass_AGameModePets_Statics; \
+public: \
+	DECLARE_CLASS(AGameModePets, AGameMode, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/PetGameUE"), NO_API) \
+	DECLARE_SERIALIZER(AGameModePets)
+
+
+#define FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AGameModePets(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AGameModePets(AGameModePets&&); \
+	NO_API AGameModePets(const AGameModePets&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AGameModePets); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AGameModePets); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AGameModePets) \
+	NO_API virtual ~AGameModePets();
+
+
+#define FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_12_PROLOG
+#define FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_SPARSE_DATA \
+	FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_ACCESSORS \
+	FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_INCLASS_NO_PURE_DECLS \
+	FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PETGAMEUE_API UClass* StaticClass<class AGameModePets>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_PetGameUE_Source_PetGameUE_Public_GameModePets_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ PetGameUE/Intermediate/Build/Win64/x64/PetGameUEEditor/Development/Core/SharedPCH.Core.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "Z:/Year Three/Module4/Unreal/PetGameUE/PetGameUE/Intermediate/Build/Win64/x64/PetGameUEEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
