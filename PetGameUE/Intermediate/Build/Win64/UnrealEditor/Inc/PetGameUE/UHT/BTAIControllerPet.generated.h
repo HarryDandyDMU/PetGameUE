de0f62a4cfad6f3867747cbe521fa4f5a9dc2bf6
Harry Dandy <P2763046@my365.dmu.ac.uk>
2025-04-25 15:01:42 +0100

    broke the entire thing

---
+++ PetGameUE/Intermediate/Build/Win64/UnrealEditor/Inc/PetGameUE/UHT/BTAIControllerPet.generated.h
@@ -0,0 +1,66 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "BTAIControllerPet.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PETGAMEUE_BTAIControllerPet_generated_h
+#error "BTAIControllerPet.generated.h already included, missing '#pragma once' in BTAIControllerPet.h"
+#endif
+#define PETGAMEUE_BTAIControllerPet_generated_h
+
+#define FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_SPARSE_DATA
+#define FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS
+#define FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_ACCESSORS
+#define FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABTAIControllerPet(); \
+	friend struct Z_Construct_UClass_ABTAIControllerPet_Statics; \
+public: \
+	DECLARE_CLASS(ABTAIControllerPet, AAIController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/PetGameUE"), NO_API) \
+	DECLARE_SERIALIZER(ABTAIControllerPet)
+
+
+#define FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABTAIControllerPet(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABTAIControllerPet(ABTAIControllerPet&&); \
+	NO_API ABTAIControllerPet(const ABTAIControllerPet&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABTAIControllerPet); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABTAIControllerPet); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABTAIControllerPet) \
+	NO_API virtual ~ABTAIControllerPet();
+
+
+#define FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_12_PROLOG
+#define FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_SPARSE_DATA \
+	FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_EDITOR_ONLY_SPARSE_DATA_PROPERTY_ACCESSORS \
+	FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_ACCESSORS \
+	FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_INCLASS_NO_PURE_DECLS \
+	FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PETGAMEUE_API UClass* StaticClass<class ABTAIControllerPet>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_PetGameUE_Source_PetGameUE_Public_BTAIControllerPet_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ PetGameUE/Source/PetGameUE/Private/BTAIControllerPet.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTAIControllerPet.h"
+
+ABTAIControllerPet::AEnemyBTAIController()
+{
+}
+
+void ABTAIControllerPet::BeginPlay()
+{
+}
+
+void ABTAIControllerPet::Tick(float DeltaTime)
+{
+}
